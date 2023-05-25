5a9193b205dc601ff0240d4d2b88e3b379251752
higgsbelly <higgsbelly@gmail.com>
2023-05-24 21:50:08 -0500

    4rd commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
-import { ConnectWallet, MediaRenderer, useContract, useValidEnglishAuctions, useMinimumNextBid } from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useContract, useValidEnglishAuctions, useMinimumNextBid} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import { NFT } from "@thirdweb-dev/sdk";
 import MinBid from "../components/MinBid"
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 const Home: NextPage = () => {
-  const { contract: marketplace, isLoading: loadingContract } = useContract(
+  const { contract, isLoading: loadingContract } = useContract(
     '0x0Aab76D12f0436c9E2C46F0C0F8406F616CeF5cc',
     "marketplace-v3"
   );
 
   const { data: auctionListing, isLoading: loadingAuction } =
-    useValidEnglishAuctions(marketplace, {
+    useValidEnglishAuctions(contract, {
       tokenContract: '0xfAFD72c9656018a60520DEb643c74eedA8199e2C',
     });
 
@@ -28,6 +29,7 @@ const Home: NextPage = () => {
           (
             <div>
               {auctionListing && auctionListing.map((nft) => {
+                
                 return (
                   <div key={nft.id} className={styles.nftContainer}>
                     <div className={styles.imageContainer}>
@@ -40,6 +42,20 @@ const Home: NextPage = () => {
                     <div className={styles.textContainer}>
                       <p>{nft.asset.name}</p>
                       <MinBid contractAddress="0x0Aab76D12f0436c9E2C46F0C0F8406F616CeF5cc" listingId={nft.id} />
+                      <p>
+                      <button
+                          onClick={async () => {
+                            try {
+                              await contract?.englishAuctions.makeBid(nft.id, 0.05);
+                            } catch (error) {
+                              console.error(error);
+                              alert(error);
+                            }
+                          }}
+                        >
+                          Make Bid
+                        </button>
+                      </p>
                     </div>
                   </div>
                 );
